0f58bf9da93367cc4ea7ad6fb1fdf825a430f19c
jkbstepien <devjakub.stepien@gmail.com>
2022-06-16 14:03:28 +0200

    part 1 done - download with curl

--- main.cpp
+++ main.cpp
@@ -23,7 +23,7 @@ void show_info() {
     std::cout << "Usage:" << std::endl;
     std::cout << "\tPress 1 to see best server player statistics." << std::endl;
     std::cout << "\tPress 2 to see 10 best server players." << std::endl;
-    std::cout << "\tPress 3 to see number of players by country." << std::endl;
+    std::cout << "\tPress 3 to see number of players by top FIDE countries." << std::endl;
     std::cout << "\tPress 4 to see number of players by FIDE title." << std::endl;
     std::cout << "\n\tPress 0 to quit program." << std::endl;
 }
@@ -55,35 +55,56 @@ void get_page(const char *url, const char *file_name) {
 }
 
 int main() {
+    // Show main program information to the user and get option from user.
     show_info();
     int option = get_user_input();
 
-    // TODO: Download data with curl.
+    // Prepare urls for downloading data from Chess.com API.
     std::string url_best_player = "https://api.chess.com/pub/player/hikaru/stats";
     std::string url_best_players_by_cat = "https://api.chess.com/pub/leaderboards";
+    std::string url_by_country = "https://api.chess.com/pub/country/";
+    std::string url_by_title = "https://api.chess.com/pub/titled/";
 
+    std::string top_11_fide_countries[11] = {"US", "RU", "RC", "IN", "UA",
+                                             "AZ", "FR", "AM", "DE", "ES", "PL"};
+    std::string fide_titles[6] = {"GM", "IM", "FM", "WGM", "WIM", "WFM"};
+
+    // Download data with curl depending on user input.
     if (option == 1) {
-        get_page(url_best_player.c_str(), "../data.json");
+        get_page(url_best_player.c_str(), "../data1.json");
     } else if (option == 2) {
-        get_page(url_best_players_by_cat.c_str(), "../data.json");
+        get_page(url_best_players_by_cat.c_str(), "../data1.json");
     } else if (option == 3) {
-        return 0;
+        for (int i = 0; i < 11; i++) {
+            std::string i_url = url_by_country + top_11_fide_countries[i] + "/players";
+            std::string i_file = "../data" + std::to_string(i + 1) + ".json";
+            std::cout << "Downloading data for coutry - " + top_11_fide_countries[i] << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+            get_page(i_url.c_str(), i_file.c_str());
+        }
     } else if (option == 4) {
-        return 0;
+        for (int i = 0; i < 6; i++) {
+            std::string i_url = url_by_title + fide_titles[i];
+            std::string i_file = "../data" + std::to_string(i + 1) + ".json";
+            std::cout << "Downloading data for title - " + fide_titles[i] << std::endl;
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+            get_page(i_url.c_str(), i_file.c_str());
+        }
     } else {
-        return 0;
+        std::cout << "Incorrect option passed. Exiting program." << std::endl;
+        exit(1);
     }
 
     // TODO: Prepare data using json.
-    json j;
-    std::ifstream i("data.json");
-    i >> j;
-
-    std::cout << "Downloading to data.json.." << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::cout << "data.json preview:" << std::endl;
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
-    std::cout << j.dump(4) << std::endl;
+//    json j;
+//    std::ifstream i("data1.json");
+//    i >> j;
+//
+//    std::cout << "Downloading to data.json.." << std::endl;
+//    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+//    std::cout << j.dump(4) << std::endl;
 
     // TODO: Visualize it in form of a graph.
 
